1a4352c1c5914c6bdf27a8db745e50f0a993b931
MarcusParkkinen <marcusparkkinen@live.com>
2016-02-04 17:45:39 +0100

    Deleted now refactored logic from the WikipediaDescriptionService. Also upped the response cache duration to 3600 seconds.

--- ArtistLookupService/src/ArtistLookupService/Controllers/ArtistLookupController.cs
+++ ArtistLookupService/src/ArtistLookupService/Controllers/ArtistLookupController.cs
@@ -19,9 +19,14 @@ namespace ArtistLookupService.Controllers
         [ServiceFilter(typeof(LogExceptionFilter))]
         [ServiceFilter(typeof(ExceptionResponseFilter))]
         [ServiceFilter(typeof(NullFilter))]
-        [ResponseCache(Duration = 10)]
+        [ResponseCache(Duration = 3600)]
         public Artist Get(string mbid)
         {
+            /*
+             *  Server-side caching would be recommended here, such as using a memory cache
+             * that stores previously requested Artists. Not currently implemented due to
+             *  time restrictions.
+             */
             return _artistDetailsService.Get(mbid);
         }
     }
--- ArtistLookupService/src/ArtistLookupService/External Service Clients/WikipediaDescriptionService.cs
+++ ArtistLookupService/src/ArtistLookupService/External Service Clients/WikipediaDescriptionService.cs
@@ -1,68 +1,35 @@
 using System;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
 using ArtistLookupService.Converters;
 using ArtistLookupService.External_Service_Interfaces;
 using ArtistLookupService.Logging;
 using ArtistLookupService.Wrappers;
-using Newtonsoft.Json;
 
 namespace ArtistLookupService.External_Service_Clients
 {
-    public class WikipediaDescriptionService : IDescriptionService
+    public class WikipediaDescriptionService : StringResourceService, IDescriptionService
     {
+        public WikipediaDescriptionService(IHttpClientWrapper httpClient, IErrorLogger logger) : base(httpClient, logger)
+        {
+        }
+
         private const string WikipediaUri = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro=true&redirects=true&titles=";
 
-        private readonly IHttpClientWrapper _httpClient;
-        private readonly IErrorLogger _logger;
-
-        public WikipediaDescriptionService(IHttpClientWrapper httpClient, IErrorLogger logger)
+        public override string CreateRequestUri(string resourceId)
         {
-            _httpClient = httpClient;
-            _logger = logger;
-        }
-
-        public async Task<string> GetAsync(string uri)
-        {
-            try
-            {
-                var transformedUri = TransformUri(uri);
-                var response = await _httpClient.GetAsync(transformedUri);
-
-                if (response.StatusCode == HttpStatusCode.OK)
-                {
-                    return await DeserializeContent(response);
-                }
-
-                _logger.Log($"Error: attempt to retrieve artist description from Wikipedia failed: {response.StatusCode} - {response.ReasonPhrase}");
-            }
-            catch (Exception ex)
-            {
-                _logger.Log(ex);
-            }
-
-            return null;
-        }
-
-        private static async Task<string> DeserializeContent(HttpResponseMessage response)
-        {
-            var json = await response.Content.ReadAsStringAsync();
-
-            return JsonConvert.DeserializeObject<string>(json, new ArtistDescriptionConverter());
-        }
-
-        private static string TransformUri(string uri)
-        {
-            if (string.IsNullOrEmpty(uri))
+            if (string.IsNullOrEmpty(resourceId))
             {
                 throw new ArgumentException($"Description Uri cannot be null or empty.");
             }
 
-            var identifier = uri.Split('/').Last();
+            var identifier = resourceId.Split('/').Last();
 
             return $"{WikipediaUri}{identifier}";
+        }
+
+        public override StringContentConverter GetConverter()
+        {
+            return new ArtistDescriptionConverter();
         }
     }
 }
